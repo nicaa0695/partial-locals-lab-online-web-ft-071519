fb384840c605a7ea89c53c8c4cca529e35989429
Veronica Lopez <nicaa0695@gmail.com>
2019-09-28 22:35:15 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,5 +14,11 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
   
-  
+   def self.search(name)
+    if name.present?
+      where('NAME like ?', "%#{name}%")
+    else
+      Student.all
+    end
+  end
 end
